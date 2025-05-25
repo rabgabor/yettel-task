61033cf554b658e2b6c362a89e66e66dd3ea42d8
rabgabor <rabgabor@me.com>
2025-05-25 12:29:51 +0200

    chore: project setup
    - Add base Xcode project
    - Set Yettel accent color
    - Extend .gitignore

---
+++ Yettel/YettelApp.swift
@@ -0,0 +1,17 @@
+//
+//  YettelApp.swift
+//  Yettel
+//
+//  Created by Rab Gábor on 2025. 05. 25..
+//
+
+import SwiftUI
+
+@main
+struct YettelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YettelTests/YettelTests.swift
@@ -0,0 +1,16 @@
+//
+//  YettelTests.swift
+//  YettelTests
+//
+//  Created by Rab Gábor on 2025. 05. 25..
+//
+
+import Testing
+
+struct YettelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
